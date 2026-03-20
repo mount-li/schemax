f99d32ddf7acccebd70ac7139e66383e15502474
Королёв Илья Михайлович <i.korolev@2gis.ru>
2026-03-20 11:14:45 +0300

    fixed type signature of Memoizer's add method

--- schemax/_memoizer.py
+++ schemax/_memoizer.py
@@ -12,7 +12,7 @@ class Memoizer(Protocol):
     A memoizer must implement some kind of a key-value storage mechanism under the hood.
     """
 
-    def add(self, key: str, obj: Any) -> None:
+    def add(self, key: str, obj: dict[str, Any]) -> None:
         """Store an object under the given key"""
         ...
 
@@ -24,7 +24,7 @@ class Memoizer(Protocol):
 class NoopMemoizer(Memoizer):
     """Memoizer that does nothing, effectively turning memoization off"""
 
-    def add(self, key: str, obj: Any) -> None:
+    def add(self, key: str, obj: dict[str, Any]) -> None:
         pass
 
     def get(self, key: str) -> dict[str, Any] | None:
